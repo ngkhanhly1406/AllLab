b9a665d2bf725658617ad5cc575e4fb138e58409
ngkhanhly1406 <111521815+ngkhanhly1406@users.noreply.github.com>
2023-06-09 15:22:17 +0700

    j

---
+++ slot8/Button.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Button : Window
+    {
+
+        public Button(int top, int left) : base(top, left)
+        {
+        }
+        // một phiên bản bị ghi đè (lưu ý từ khóa) bởi vì trong 
+        // phương thức dẫn xuất, chúng tôi thay đổi hành vi
+        public override void DrawWindow()
+        {
+            Console.WriteLine("Drawing a button at {0}, {1}\n", top, left);
+        }
+    }
+}
+
