ab1ba78837fffcf9ffb70dfbd38728d8ec477da0
ngkhanhly1406 <111521815+ngkhanhly1406@users.noreply.github.com>
2024-03-26 10:47:20 +0700

    baikiemtra

---
+++ EXAM112/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EXAM112.Models
+{
+    public class Employee
+    {
+
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string Email{ get; set; }
+    }
+}
+
---
+++ EXAM112/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿
+using EXAM112.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class EmployeeContext : DbContext
+{
+    public EmployeeContext(DbContextOptions<EmployeeContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; } = null!;
+}
